6fd8fa22fe7a78e19bcc883b5334a8f315d0894c
Zixuan Zhang <zhan7230@umn.edu>
2020-01-26 11:10:57 -0600

    Add Recursion Questions

--- leetcode-questions/Array/268_missing_number.py
+++ leetcode-questions/Array/268_missing_number.py
@@ -1,3 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return sum([x for x in range(len(nums) + 1)]) - sum(nums)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return int(n * (n + 1) / 2 - sum(nums))

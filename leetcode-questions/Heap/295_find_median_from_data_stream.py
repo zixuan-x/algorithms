04056e2fe5a7236e195ab509841f7acbee3f7cdf
Zixuan Zhang <zhan7230@umn.edu>
2020-02-29 18:56:07 -0600

    Add heap files

---
+++ leetcode-questions/Heap/295_find_median_from_data_stream.py
@@ -0,0 +1,29 @@
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.small = [] # max heap
+        self.large = [] # min heap
+
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+        else:
+            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+    
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return (-self.small[0] + self.large[0]) / 2
+        else:
+            return self.large[0]
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()

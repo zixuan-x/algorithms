10d98b567fb438b0d2825256cf5dc952573bcb85
Zixuan Zhang <zhan7230@umn.edu>
2020-03-11 12:48:04 -0500

    Add python files

--- leetcode-questions/Graph/399_evaluate_division.py
+++ leetcode-questions/Graph/399_evaluate_division.py
@@ -1,5 +1,43 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def find(x): # X / root = res
+            if parents[x][0] != x:
+                rx, vx = find(parents[x][0])
+                parents[x] = (rx, vx * parents[x][1])
+            return parents[x]
+        
+        # def union(x, y):
+        def divide(x, y):
+            rx, vx = find(x)
+            ry, vy = find(y)
+            if rx != ry:
+                return -1.0
+            return vx / vy
+        
+        parents = {} # A -> B, cost = A / B
+        
+        # iterate through equations to union nodes
+        for (x, y), v in zip(equations, values):
+            if x not in parents and y not in parents:
+                parents[x] = (y, v)
+                parents[y] = (y, 1.0)
+            elif x not in parents:
+                parents[x] = (y, v)
+            elif y not in parents:
+                parents[y] = (x, 1.0 / v)
+            else:
+                rx, vx = find(x) # x / root1 = vx
+                ry, vy = find(y) # y / root2 = vy
+                                 # x / y = v
+                # want: root1 / root2 = ?
+                parents[rx] = (ry, v * vy / vx) # v * vy / vx = (x / root2) * (root1 / x)
+            
+        # iterate queries to get resultls
+        return [divide(x, y) if x in parents and y in parents else -1.0 for x, y in queries]
+        
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         # construct graph
         g = {} #{ node : { node : weight } }
         # g[A][B] 
--- leetcode-questions/Graph/547_friend_cycle.py
+++ leetcode-questions/Graph/547_friend_cycle.py
@@ -1,6 +1,41 @@
 class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
         n = len(M)
+        parents = [i for i in range(n)]
+        ranks = [1 for _ in range(n)]
+        
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px == py: return
+            if ranks[px] < ranks[py]:
+                parents[px] = py
+            elif ranks[px] > ranks[py]:
+                parents[py] = px
+            else:
+                parents[px] = py
+                ranks[py] += 1
+        
+        # iterate through all nodes and union friends
+        for i in range(n):
+            for j in range(n):
+                if M[i][j]:
+                    union(i, j)
+                
+        # find out how many roots there are
+        count = 0
+        for i in range(n):
+            if parents[i] == i:
+                count += 1
+        return count
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)
         visited = [0] * n
         count = 0
         for i in range(n):

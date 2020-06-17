f06c22456229716c0fa0491d0349faafeea4c80d
Zixuan Zhang <zhan7230@umn.edu>
2020-06-17 14:29:50 -0500

    Add Graph files

--- leetcode/Graph/Dijkstra/743. Network Delay Time.py
+++ leetcode/Graph/Dijkstra/743. Network Delay Time.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from heapq import heappush, heappop
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
@@ -7,11 +8,12 @@ class Solution:
             graph[start].append((end, time))
         visited = {} # {node: time from src}
         
-        heap = [(0, K)] # (time, node)
+        heap = [(0, K)] # [(time, node)]
         while heap:
-            time, node = heapq.heappop(heap)
+            time, node = heappop(heap)
             if node not in visited:
                 visited[node] = time
                 for neighbor, t in graph[node]:
-                    heapq.heappush(heap, (time + t, neighbor))
+                    heappush(heap, (time + t, neighbor))
         return max(visited.values()) if len(visited) == N else -1
+

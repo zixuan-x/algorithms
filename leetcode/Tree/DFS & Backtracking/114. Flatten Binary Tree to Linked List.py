ab864a4fe52139686fa1d08e5526dd031a495eb3
Zixuan Zhang <zhan7230@umn.edu>
2020-06-22 21:40:52 -0500

    Add tree problems

---
+++ leetcode/Tree/DFS & Backtracking/114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    """
+    postorder traversal from right to left
+    use prev to connect nodes together
+    remember to set root.left to None
+    """
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.dfs(root, [None])
+        
+    def dfs(self, root, prev):
+        if not root:
+            return
+        else:
+            self.dfs(root.right, prev)
+            self.dfs(root.left, prev)
+            root.right = prev[0]
+            root.left = None
+            prev[0] = root
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        self.dfs(root, None)
+        
+
+    def dfs(self, root, tailHead):
+        if not root:
+            return tailHead
+        
+        tailHead = self.dfs(root.right, tailHead)
+        tailHead = self.dfs(root.left, tailHead)
+        
+        root.right = tailHead
+        root.left = None
+        tailHead = root
+        return tailHead

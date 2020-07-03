517ac759482edb50b53cdf7d60ea2b3ca4ebdfba
Zixuan Zhang <zhan7230@umn.edu>
2020-07-03 12:26:54 -0500

    Add tree problem

---
+++ leetcode/Tree/BFS/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        levels = []
+        queue = deque([root])
+        while queue:
+            levelSize = len(queue)
+            level = []
+            for _ in range(levelSize):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            levels.append(level)
+        return levels
+
+
+        

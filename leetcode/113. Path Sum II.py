d38efb018944b19f89bd5594780c5d5b25e926d5
Zixuan Zhang <zhan7230@umn.edu>
2020-07-26 20:28:16 -0500

    Add tree problems

--- leetcode/113. Path Sum II.py
+++ leetcode/113. Path Sum II.py
@@ -1,28 +1,26 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
-        res = []
-        self.dfs(root, sum, [], res)
-        return res
-    
-    def dfs(self, root, sum, path, res):
+        if not root:
+            return []
+        
+        result = []
+        self.search(root, sum, [], result)
+        return result
+        
+    def search(self, root, sum, path, result):
         if not root:
             return
         
         path.append(root.val)
         if not root.left and not root.right and root.val == sum:
-            res.append(path[:])
-            # path.pop()
-            # return
-        
-        self.dfs(root.left, sum - root.val, path, res)
-        self.dfs(root.right, sum - root.val, path, res)
-        
-        path.pop()
+            result.append(path[:])
             
+        self.search(root.left, sum - root.val, path, result)
+        self.search(root.right, sum - root.val, path, result)
+        path.pop()

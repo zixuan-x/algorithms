68e1f948676fe4ff9009d2e8231aa146a81e1132
Zixuan Zhang <zhan7230@umn.edu>
2020-07-27 19:12:28 -0500

    Add tree problems

--- leetcode/257. Binary Tree Paths.py
+++ leetcode/257. Binary Tree Paths.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 ''' 1. Backtracking '''
 class Solution:
@@ -25,7 +25,34 @@ class Solution:
         self.dfs(root.right, path, res)
         path.pop()
 
+
 ''' 2. BFS '''
+def binaryTreePaths2(self, root):
+    if not root:
+        return []
+    res, queue = [], collections.deque([(root, "")])
+    while queue:
+        node, ls = queue.popleft()
+        if not node.left and not node.right:
+            res.append(ls+str(node.val))
+        if node.left:
+            queue.append((node.left, ls+str(node.val)+"->"))
+        if node.right:
+            queue.append((node.right, ls+str(node.val)+"->"))
+    return res
 
 
 ''' 3. Backtracking with Stack '''
+def binaryTreePaths1(self, root):
+    if not root:
+        return []
+    res, stack = [], [(root, "")]
+    while stack:
+        node, ls = stack.pop()
+        if not node.left and not node.right:
+            res.append(ls+str(node.val))
+        if node.right:
+            stack.append((node.right, ls+str(node.val)+"->"))
+        if node.left:
+            stack.append((node.left, ls+str(node.val)+"->"))
+    return res

5b191544260c994dbc79f8a5ce2c8c6ae4e74a31
Zixuan Zhang <zhan7230@umn.edu>
2020-08-06 00:48:48 -0500

    Add leetcode problem

--- leetcode/139. Word Break.py
+++ leetcode/139. Word Break.py
@@ -1,3 +1,36 @@
+'''
+1. memoization search - top down
+
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        if not s: return True
+        if not wordDict: return False
+        
+        wordDict = set(wordDict)
+        m = len(max(wordDict, key=len))
+        return self.memoSearch(s, wordDict, {}, m)
+        
+    def memoSearch(self, s, wordDict, memo, m):
+        # base case
+        if not s or s in wordDict:
+            return True
+        
+        # recursive rules
+        if s not in memo:
+            isValid = False
+            for i in range(min(m, len(s))):
+                if s[:i+1] in wordDict and self.memoSearch(s[i+1:], wordDict, memo, m):
+                    isValid = True
+                    break
+            memo[s] = isValid
+        return memo[s]
+
+
+'''
+2. dynamic programming - bottom up
+
+'''
 class Solution:
     """
     1. dp(i) -> True if s[0:i] can be segmented into words, otherwise false

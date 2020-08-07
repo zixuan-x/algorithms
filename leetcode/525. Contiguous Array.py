994b9f90bab60b8f2d11fb6e38092af2b5f33f75
Zixuan Zhang <zhan7230@umn.edu>
2020-08-07 15:07:01 -0500

    Add leetcode problem

---
+++ leetcode/525. Contiguous Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        count = 0
+        maxLength = 0
+        table = {0: -1}
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count -= 1
+            else:
+                count += 1
+            
+            if count in table:
+                maxLength = max(maxLength, i - table[count])
+            else:
+                table[count] = i
+        return maxLength
+            

cd0ab39e567f337f2e242dc8fa3d74a4741cfea9
Zixuan Zhang <zhan7230@umn.edu>
2020-08-13 00:35:01 -0500

    Add leetcode problems

---
+++ leetcode/242. Valid Anagram.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)

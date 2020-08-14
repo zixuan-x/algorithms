ae43643973c713b679d224662cd4f3df0a7d3ce9
Zixuan Zhang <zhan7230@umn.edu>
2020-08-14 01:10:32 -0500

    Add leetcode problem

--- leetcode/387. First Unique Character in a String.py
+++ leetcode/387. First Unique Character in a String.py
@@ -1,3 +1,28 @@
+'''
+bits
+'''
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        seen, duplicate = 0, 0
+        for c in s:
+            index = ord(c) - ord('a')
+            musk = 1 << index
+            if seen & musk > 0:
+                duplicate |= musk 
+            else:
+                seen |= musk
+        
+        for i in range(len(s)):
+            index = ord(s[i]) - ord('a')
+            musk = 1 << index
+            if not duplicate & musk:
+                return i
+            
+        return -1
+
+'''
+hash table - counter
+'''
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         counter = Counter(s)
@@ -5,3 +30,6 @@ class Solution:
             if counter[s[i]] == 1:
                 return i
         return -1
+
+
+            

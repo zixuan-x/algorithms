7f677a824e88251aca2adf92b607128a9b184471
Zixuan Zhang <zhan7230@umn.edu>
2020-08-14 01:10:22 -0500

    Add leetcode problem

--- leetcode/253. Meeting Rooms II.py
+++ leetcode/253. Meeting Rooms II.py
@@ -1,10 +1,19 @@
+'''
+Every time you need to start a meeting, 
+you try to search through all existing rooms and see if there's an empty one,
+if you couldn't find one,
+open a new room
+'''
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        minHeap = []
+        minHeap = []  # stores the end time of intervals
+        start, end = 0, 1
         for interval in intervals:
-            if minHeap and minHeap[0] <= interval[0]:
-                heapq.heapreplace(minHeap, interval[1])
+            if not minHeap or interval[start] < minHeap[0]:
+                heappush(minHeap, interval[end])
             else:
-                heapq.heappush(minHeap, interval[1])
+                # heapreplace(minHeap, interval[end])
+                heappop(minHeap)
+                heappush(minHeap, interval[end])
         return len(minHeap)

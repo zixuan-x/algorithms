4e63bf7e174b15870f40c9705b421ed7c498c75c
Zixuan Zhang <zhan7230@umn.edu>
2020-08-15 00:45:09 -0500

    Add leetcode problem

---
+++ leetcode/451. Sort Characters By Frequency.py
@@ -0,0 +1,42 @@
+'''
+Python Cheating Solution
+'''
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c = Counter(s)
+        s = sorted(list(s), key=lambda x: (c[x], x), reverse=True)
+        return ''.join(s)
+
+'''
+bucket sort: (frequency bucket)
+'''
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c = Counter(s)
+        
+        frequencies = defaultdict(list)  # {frequency: []}
+        for char, times in c.items():
+            frequencies[times] += [char] * times
+        
+        result = []
+        for times in range(len(s), 0, -1):
+            result += frequencies[times]
+        return ''.join(result)
+
+'''
+heap
+'''
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        c = Counter(s)
+        
+        maxHeap = []
+        for char, times in c.items():
+            heappush(maxHeap, (-times, char))
+        
+        result = []
+        while maxHeap:
+            times, char = heappop(maxHeap)
+            times = -times
+            result += [char] * times
+        return ''.join(result)

68fc6ee4ecb5a2c7741cf66b27e26b7e0773ce30
Zixuan Zhang <zhan7230@umn.edu>
2020-08-17 10:36:24 -0500

    Add leetcode problem

--- leetcode/394. Decode String.py
+++ leetcode/394. Decode String.py
@@ -1,21 +1,47 @@
+'''
+two stack:
+time: O(n) where n is the result string
+space: O(n)
+'''
 class Solution:
     def decodeString(self, s: str) -> str:
-        repeat_stack = []
-        str_stack = []
-        cur_str = ''
-        cur_num = 0
-        
+        cur = ''
+        number = 0
+        numberStack = []
+        stringStack = []
         for c in s:
             if c.isdigit():
-                cur_num = cur_num * 10 + int(c)
+                number = number * 10 + int(c)
+            elif c.isalpha():
+                cur += c
             elif c == '[':
-                repeat_stack.append(int(cur_num))
-                str_stack.append(cur_str)
-                cur_str = ''
-                cur_num = 0
-            elif c == ']':
-                cur_str = str_stack.pop() + cur_str * repeat_stack.pop()
-            else:
-                cur_str += c
-        
-        return cur_str
+                numberStack.append(number)
+                number = 0
+                stringStack.append(cur)
+                cur = ''
+            else:  # c == ']'
+                cur = stringStack.pop() + cur * numberStack.pop()    
+        return cur
+
+'''
+one stack:
+'''
+class Solution:
+    def decodeString(self, s: str) -> str:
+        cur = ''
+        number = 0
+        stack = []
+        for c in s:
+            if c.isdigit():
+                number = number * 10 + int(c)
+            elif c.isalpha():
+                cur += c
+            elif c == '[':
+                stack.append(cur)
+                stack.append(number)
+                number = 0
+                cur = ''
+            else:  # c == ']'
+                num = stack.pop()
+                cur = stack.pop() + cur * num
+        return cur

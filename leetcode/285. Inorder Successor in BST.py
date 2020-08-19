10f2e3bfc02415b1744cef334d64882a8faab015
Zixuan Zhang <zhan7230@umn.edu>
2020-08-19 02:18:20 -0500

    Add leetcode problems

--- leetcode/285. Inorder Successor in BST.py
+++ leetcode/285. Inorder Successor in BST.py
@@ -5,6 +5,17 @@
 #         self.left = None
 #         self.right = None
 
+class Solution:
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':
+        node, succ = root, None
+        while node:
+            if p.val < node.val:
+                succ = node
+                node = node.left
+            else:
+                node = node.right
+        return succ
+
 ''' Successor '''
 class Solution:
     def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'TreeNode':

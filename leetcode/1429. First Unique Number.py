6a72ca5f6b976d7b9c67f83c762047cf5066dff0
Zixuan Zhang <zhan7230@umn.edu>
2020-08-21 12:06:59 -0500

    Add leetcode problems

--- leetcode/1429. First Unique Number.py
+++ leetcode/1429. First Unique Number.py
@@ -1,4 +1,64 @@
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.perv = None
+        self.next = None
 
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.head = Node(-1)
+        self.head.next = self.head
+        self.head.prev = self.head
+        self.seen = {}
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self) -> int:
+        if self.head.next != self.head:
+            return self.head.next.val
+        return -1
+        
+    def add(self, value: int) -> None:
+        if value in self.seen:
+            node = self.seen[value]
+            if node:
+                prev, next = node.prev, node.next
+                prev.next = next
+                next.prev = prev
+                self.seen[value] = None
+        else:
+            node = Node(value)
+            self.seen[value] = node
+            node.prev = self.head.prev
+            node.next = self.head
+            node.prev.next = node
+            node.next.prev = node  
+
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.seen = set()
+        self.unique = {}
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self) -> int:
+        # for n in self.unique.keys():
+        for n in self.unique:
+            return n
+        return -1
+        # return next(iter(self.unique.keys()), -1)
+        # return next(iter(self.unique), -1)
+
+    def add(self, value: int) -> None:
+        if value in self.seen:
+            if value in self.unique:
+                del self.unique[value]
+        else:
+            self.seen.add(value)
+            self.unique[value] = None
+        
 
 class FirstUnique:
 

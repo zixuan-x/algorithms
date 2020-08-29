af94bfb6e0b9bafb6a194c7971bdc6041679d489
Zixuan Zhang <zhan7230@umn.edu>
2020-08-29 11:03:21 -0500

    Add leetcode problems

--- leetcode/150. Evaluate Reverse Polish Notation.py
+++ leetcode/150. Evaluate Reverse Polish Notation.py
@@ -1,6 +1,48 @@
 '''
 面试中要问清楚除法处理的规则
+recursion:
 '''
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        t = tokens.pop()
+        
+        if t not in ['+', '-', '*', '/']:
+            return int(t)
+        
+        right, left = self.evalRPN(tokens), self.evalRPN(tokens)
+        
+        if t == '+':
+            return left + right
+        if t == '-':
+            return left - right
+        if t == '*':
+            return left * right
+        if t == '/':
+            return int(left / right)
+
+'''
+面试中要问清楚除法处理的规则
+stack:
+'''
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        values = []
+        for token in tokens:
+            if token in ['+', '-', '*', '/']:
+                right, left = values.pop(), values.pop()
+                result = 0
+                if token == '+':
+                    result = left + right
+                elif token == '-':
+                    result = left - right
+                elif token == '*':
+                    result = left * right
+                elif token == '/':
+                    result = int(left / right)
+                values.append(result)
+            else:
+                values.append(int(token))
+        return values.pop()
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:

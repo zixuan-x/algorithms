b832bd596cd91c963f0fb37c72853bc930d4c269
Zixuan Zhang <zhan7230@umn.edu>
2020-08-29 11:03:24 -0500

    Add leetcode problems

--- leetcode/242. Valid Anagram.py
+++ leetcode/242. Valid Anagram.py
@@ -5,3 +5,15 @@ class Solution:
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter = [0] * 26 # You may assume the string contains only lowercase alphabets.
+        for c in s:
+            counter[ord(c) - 97] += 1
+        for c in t:
+            counter[ord(c) - 97] -= 1
+        for count in counter:
+            if count:
+                return False
+        return True
